7140792a5727cdbb776e6aa4296b8d2c66883003
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-09-02 20:45:10 -0300

    Verifica dentre as 3 variaveis se existe um numero impar

---
+++ bloco_4/dia_1/oddNumbers/oddNumbers3.js
@@ -0,0 +1,11 @@
+let a = 3;
+let b = 5;
+let c = 7;
+let oddNumbers = false;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    oddNumbers = true;
+    console.log(oddNumbers);
+} else {
+    console.log("Não há números ímpares dentre os 3");
+}
