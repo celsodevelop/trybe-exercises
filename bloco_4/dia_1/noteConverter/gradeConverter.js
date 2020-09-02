d07e805094a68628742dd427ad2828ca9d5ef74e
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-09-02 20:35:34 -0300

    Refactoring for B grade !

--- bloco_4/dia_1/noteConverter/gradeConverter.js
+++ bloco_4/dia_1/noteConverter/gradeConverter.js
@@ -1,33 +1,26 @@
-let a =30;
+let a =80;
 let conceito = "";
 
 if (a < 50) {
     conceito = "F";
-    break;
 } 
 else if (a >= 50 && a < 60) {
     conceito = "E";
-    break;
 }
 else if (a >= 60 && a < 70){
     conceito = "D";
-    break;
 }
 else if (a >= 70 && a < 80){
     conceito : "C";
-    break;
 }
 else if (a >= 80 && a < 90 ){
     conceito = "B";
-    break;
 }
 else if (a >= 90){
     conceito = "A";
-    break;
 }
 else {
     conceito = "Insira uma nota valida";
-    break;
 }
 
 console.log (conceito);
