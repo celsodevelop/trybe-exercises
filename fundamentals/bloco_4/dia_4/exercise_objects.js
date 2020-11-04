5a2fc7ae5c006752e64ee0c5149758aabba61c9b
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-11-04 15:36:38 -0300

    1 Desejando boasVindas à personagem registrada no objeto info

---
+++ fundamentals/bloco_4/dia_4/exercise_objects.js
@@ -0,0 +1,13 @@
+const info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// Imprima no console uma mensagem de boas-vindas para a personagem acima incluindo seu nome
+
+const boasVindas = (nome) => `Seja muito bem vinda ${nome}`;
+
+console.log(boasVindas(info.personagem));
+
+const recurrenceSet
