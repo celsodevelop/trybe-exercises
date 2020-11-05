0aa33e5466d83c5569a41e0bc37395b47a4e8f37
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-11-04 22:45:24 -0300

    Trabalhando com Objetos

---
+++ fundamentals/bloco_4/dia_4/README.md
@@ -0,0 +1,41 @@
+# DIA 4
+
+## DESCRIÇÃO DOS PROGRAMAS
+
+### Soluções 1 - Objects e For/in no arquivo JS:
+
+`exercise_objects.js`
+
+Partindo do objeto:
+
+ ```javascript
+ let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+  ```
+
+### Exercício 1
+
+Exibir mensagem de boas vindas . Função boasVindas(nome) devolve console.log desejando boas vindas à personagem.
+
+### Exercício 2
+
+Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+
+### Exercício 3
+
+Faça um for/in que mostre todas as chaves do objeto.
+
+### Exercício 4
+
+Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+### Exercício 5
+
+Agora, defina um segundo objeto com a mesma estrutura, as mesmas chaves do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+### Soluções Funções no arquivo JS:
+
+`exercise_functions.js`
--- fundamentals/bloco_4/dia_4/exercise_objects.js
+++ fundamentals/bloco_4/dia_4/exercise_objects.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
 const info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -6,8 +8,65 @@ const info = {
 
 // Imprima no console uma mensagem de boas-vindas para a personagem acima incluindo seu nome
 
-const boasVindas = (nome) => `Seja muito bem vinda ${nome}`;
+const boasVindas = (nome) => `Seja muito bem vinda ${nome} \n`;
 
-console.log(boasVindas(info.personagem));
+// console.log(boasVindas(info.personagem)); // Exercício 1
 
-const recurrenceSet
+/*  Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e,
+em seguida, imprima o objeto no console. */
+
+const recurrenceSet = (character, boolData) => {
+  const infotrg = character;
+  infotrg.recorrente = boolData;
+};
+
+// recurrenceSet(info, 'Sim'); // Exercício 2
+
+((obj) => {
+  for (const properties in obj) {
+    console.log(properties);
+  }
+})(info);
+
+console.log('Chaves: \n');
+
+// viewKeys(info); // Exercício 3
+
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+const viewValues = (obj) => {
+  for (const key in obj) {
+    console.log(obj[key]);
+  }
+};
+
+console.log('\nValores: \n');
+
+// viewValues(info) // Exercício 4
+
+const newobj = (template) => {
+  const output = {};
+  let i = 0;
+  for (const key in template) {
+    const values = ['Tio Patinhas', 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178', 'O último MacPatinhas', 'Sim'];
+    output[key] = values[i];
+    i += 1;
+  }
+  return output;
+};
+
+const info2 = newobj(info);
+
+const compareTwoObj = ({ recorrente: data1 }, { recorrente: data2 }) => (data1 === data2);
+
+function compareTwo() {
+  const output = {};
+  if (compareTwoObj(info, info2)) {
+    for (const key in info) {
+      output[key] = `${info[key]} e ${info2[key]}`;
+      (key == 'recorrente') ? (output[key] = 'Ambos recorrentes') :(null);
+    }
+  }
+  return output;
+}
+// console.log(compareTwo()); // Exercício 5
