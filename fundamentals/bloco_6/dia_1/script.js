80740b77b219949d03b1edee832c9e5d6032b3a6
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-12-02 19:23:28 -0300

    realizada funcoes que validam comprimento do texto e campos obrigatórios preenchidos

--- fundamentals/bloco_6/dia_1/script.js
+++ fundamentals/bloco_6/dia_1/script.js
@@ -26,5 +26,48 @@ const populateStates = () => {
       });
     });
 };
+const capturaEvento = (idElemento, tipoEvento, callback) => {
+  const elementoDOM = document.getElementById(idElemento);
+  elementoDOM.addEventListener(tipoEvento, (elementoEvento) => {
+    callback(elementoEvento);
+  });
+};
+const validaFormulario = (elementoEvento) => {
+  let output = true;
+  output = validaCamposObrigatorios();
+  output = validaLimiteTexto();
+  if (output) {
+    console.log('output = true');
+  }
+  elementoEvento.preventDefault();
+};
+const validaCamposObrigatorios = () => {
+  let output = true;
+  const requiredInputs = document.querySelectorAll('[required]');
+  requiredInputs.forEach(input => {
+    if (input.validity.valueMissing) {
+      const nomeInput = document.querySelector(`label[for=${input.id}]`);
+      console.log(`${nomeInput.innerText} é um campo obrigatório e não pode ser vazio!`);
+    }
+  });
+  return output;
+};
+const validaLimiteTexto = () => {
+  let output = true;
+  const requiredInputs = document.querySelectorAll('[required]');
+  requiredInputs.forEach(input => {
+    console.log(input.validity.tooLong);
+    console.log(input.validity.tooShort);
+    const nomeInput = document.querySelector(`label[for=${input.id}]`);
+    if (input.validity.tooLong) {
+      console.log(`${nomeInput.innerText} tem um número de ${input.getAttribute('maxlength')} caracteres máximo`);
+    }
+    if (input.validity.tooShort) {
+      console.log(`${nomeInput.innerText} tem um numéro de ${input.getAttribute('minlength')} caracteres mínimo`);
+    }
+  });
+  return output;
+};
 
 populateStates();
+capturaEvento('button-send', 'click', validaFormulario);
