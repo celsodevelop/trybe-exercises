2454c6036664c52cda020fdfeaa33f273e8e8885
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-12-03 11:37:01 -0300

    refatorando para adicionar maior complexidade sem afetar a legibilidade

--- fundamentals/bloco_6/dia_1/script.js
+++ fundamentals/bloco_6/dia_1/script.js
@@ -41,52 +41,63 @@ const testesFormularioCurriculo = (input) => ({
 const criaAviso = (input, nameTest) => {
   const nomeInput = document.querySelector(`label[for=${input.id}]`);
   const toTest = testesFormularioCurriculo(input);
-  const msg = document.createElement('span');
-  msg.className = 'error-msg';
-  msg.id = `${input.id}-error-msg`;
-  if (document.getElementById(msg.id)) { input
-    .parentNode
-    .removeChild(document.getElementById(msg.id));
-  }
-  msg.style.color = 'red';
-  msg.innerText = `${nomeInput.innerText} ${toTest[nameTest]}`;
-  input.insertAdjacentElement('afterend', msg);
+  const errorMsg = document.createElement('p');
+  errorMsg.className = 'error-msg';
+  errorMsg.id = `${input.id}-error`;
+  errorMsg.style.color = 'red';
+  errorMsg.innerText = `${nomeInput.innerText} ${toTest[nameTest]}`;
+  input.insertAdjacentElement('afterend', errorMsg);
 };
-const criaDiv = () => {
+const processaElemento = (input) => {
+  const nomeInput = document.querySelector(`label[for=${input.id}]`);
+  const newData = document.createElement('p');
+  if (input.value) {
+    newData.className = `${input.name}-data`;
+    newData.id = `${input.name}-data`;
+    newData.innerText = `${nomeInput.innerText}: ${input.value}`;
+  }
+  return newData;
+};
+const processaFormulario = (aposBotao) => {
   const formData = document.querySelectorAll('form.job-form input');
   const divData = document.createElement('div');
   divData.className = 'div-data';
   divData.id = 'div-data';
   divData.innerHTML = '<p class="data-title">Dados cadastrados com sucesso!</p><br>';
   formData.forEach((input) => {
-    if (input.value) {
-      const nomeInput = document.querySelector(`label[for=${input.id}]`);
-      console.log(input.value);
-      const newData = document.createElement('p');
-      newData.className = `${input.name}-data`;
-      newData.id = `${input.name}-data`;
-      newData.innerText = `${nomeInput.innerText}: ${input.value}`;
-      divData.appendChild(newData);
+    const newData = processaElemento(input);
+    divData.appendChild(newData);
+  });
+  aposBotao.target.nextElementSibling.insertAdjacentElement('afterend', divData);
+};
+const clearErrorMsg = (element) => {
+  const errorMsg = `${element.id}-error`;
+  if (document.getElementById(errorMsg)) {
+    element.parentNode.removeChild(document.getElementById(errorMsg));
+  }
+};
+const validaElemento = (input, toTest) => {
+  let isValid = true;
+  const keysTest = Object.keys(toTest);
+  clearErrorMsg(input);
+  keysTest.forEach((nameTest) => {
+    if (input.validity[nameTest]) {
+      criaAviso(input, nameTest);
+      isValid = false;
     }
   });
-  console.log(divData);
-  document.body.appendChild(divData);
+  return isValid;
 };
-const validacoesFormulario = (eventoBotao) => {
+const validaFormulario = (eventoBotao) => {
   eventoBotao.preventDefault();
   let isValid = true;
   const requiredInputs = document.querySelectorAll('[required]');
   requiredInputs.forEach((input) => {
     const toTest = testesFormularioCurriculo(input);
-    const keysTest = Object.keys(toTest);
-    keysTest.forEach((nameTest) => {
-      if (input.validity[nameTest]) {
-        isValid = false;
-        criaAviso(input, nameTest);
-      }
-    });
+    isValid = validaElemento(input, toTest);
   });
-  if (isValid) criaDiv();
+  if (isValid) processaFormulario(eventoBotao);
 };
+
 populateStates();
-capturaEvento('button-send', 'click', validacoesFormulario);
+capturaEvento('button-send', 'click', validaFormulario);
