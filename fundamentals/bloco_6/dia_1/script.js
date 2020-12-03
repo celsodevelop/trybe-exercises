7ccca5e96d59d0b7d2b2148e4a0093c80b86418d
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-12-03 03:31:02 -0300

    better refactoring custom error msg when check form setup

--- fundamentals/bloco_6/dia_1/script.js
+++ fundamentals/bloco_6/dia_1/script.js
@@ -1,10 +1,10 @@
 const buscaEstados = async () => {
   let output = [];
   await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-    .then(response => response.json())
-    .then(states => states.forEach(uf => {
-      output.push(uf.sigla);
-    }));
+  .then(response => response.json())
+  .then(states => states.forEach(uf => {
+    output.push(uf.sigla);
+  }));
   output = output.sort();
   return output;
 };
@@ -19,12 +19,12 @@ const criarOption = (item) => {
 const populateStates = () => {
   const selectElement = document.getElementById('states-input');
   buscaEstados()
-    .then(states => {
-      states.forEach((uf) => {
-        const novaOption = criarOption(uf);
-        selectElement.appendChild(novaOption);
-      });
+  .then(states => {
+    states.forEach((uf) => {
+      const novaOption = criarOption(uf);
+      selectElement.appendChild(novaOption);
     });
+  });
 };
 const capturaEvento = (idElemento, tipoEvento, callback) => {
   const elementoDOM = document.getElementById(idElemento);
@@ -32,51 +32,49 @@ const capturaEvento = (idElemento, tipoEvento, callback) => {
     callback(elementoEvento);
   });
 };
-const validaFormulario = (elementoEvento) => {
-  let output = true;
-  output = validaCamposObrigatorios();
-  output = validaLimiteTexto();
-  if (output) {
-    console.log('output = true');
-  }
-  elementoEvento.preventDefault();
-};
+// const validaFormulario = (elementoEvento) => {
+//   let output = true;
+//   output = validaCamposObrigatorios();
+//   output = validaLimiteTexto();
+//   if (output) {
+//     console.log('output = true');
+//   }
+//   elementoEvento.preventDefault();
+// };
 const testesFormularioCurriculo = (input) => ({
-  valueMissing: () => ' é um campo obrigatório e não pode ser vazio!',
-  tooLong: () => ` tem um número de ${input.getAttribute('maxlength')} caracteres máximo!`,
-  tooShort: () => ` tem um número de ${input.getAttribute('minlength')} caracteres mínimo!`,
-  patternMismatch: () => {
-    let msg = ' deve ter um padrão válido "username@example.com"!';
-    if (input.id === 'job-date-input') msg = ' deve estar no formato dd/mm/aaaa !';
-    return msg;
-  },
+  valueMissing: 'é um campo obrigatório e não pode ser vazio!',
+  tooLong: `tem um número de ${input.getAttribute('maxlength')} caracteres máximo!`,
+  tooShort: `tem um número de ${input.getAttribute('minlength')} caracteres mínimo!`,
+  patternMismatch: `deve ter um padrão válido ${input.getAttribute('placeholder')}!`,
 });
 
-const validacoesFormulario = () => {
+const validacoesFormulario = (eventoBotao) => {
+  eventoBotao.preventDefault();
   let isValid = true;
   const requiredInputs = document.querySelectorAll('[required]');
   requiredInputs.forEach((input) => {
     const toTest = testesFormularioCurriculo(input);
     const keysTest = Object.keys(toTest);
-    console.log(keysTest);
+    // console.log(keysTest);
     keysTest.forEach((nameTest) => {
-      if (nameTest) {
-        console.log(nameTest);
-        const nomeInput = document.querySelector(`label[for=${input.id}]`);
-        console.log(input.validity[nameTest]);
-        if (input.validity[nameTest]) {
-          console.log(`${nomeInput.innerText}${toTest[nameTest]()}`);
-        }
+      // console.log(nameTest);
+      const nomeInput = document.querySelector(`label[for=${input.id}]`);
+      // console.log(input.validity[nameTest]);
+      if (input.validity[nameTest]) {
+        // console.log(nameTest);
+        isValid = false;
+        console.log(`${nomeInput.innerText} ${toTest[nameTest]}`);
       }
     });
   });
-// valueMissing
-// tooLong
-// tooShort
-// patternMismatch
+  return isValid;
+  // valueMissing
+  // tooLong
+  // tooShort
+  // patternMismatch
 };
 // validacoesFormulario();
-const validaCamposObrigatorios = () => {
+/* const validaCamposObrigatorios = () => {
   const output = true;
   const requiredInputs = document.querySelectorAll('[required]');
   requiredInputs.forEach(input => {
@@ -95,14 +93,16 @@ const validaLimiteTexto = () => {
     console.log(input.validity.tooShort);
     const nomeInput = document.querySelector(`label[for=${input.id}]`);
     if (input.validity.tooLong) {
-      console.log(`${nomeInput.innerText} tem um número de ${input.getAttribute('maxlength')} caracteres máximo`);
+      console.log(`${nomeInput.innerText} tem um número de ${input.getAttribute('maxlength')}
+      caracteres máximo`);
     }
     if (input.validity.tooShort) {
-      console.log(`${nomeInput.innerText} tem um numéro de ${input.getAttribute('minlength')} caracteres mínimo`);
+      console.log(`${nomeInput.innerText} tem um numéro de ${input.getAttribute('minlength')}
+      caracteres mínimo`);
     }
   });
   return output;
-};
+}; */
 
 populateStates();
 capturaEvento('button-send', 'click', validacoesFormulario);
