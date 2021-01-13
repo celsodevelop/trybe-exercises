27db0c2d81d72e1ab53dbb454265d367c2985419
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-01-13 09:54:47 -0300

    feat: first react exercise ready, printing list

--- front-end/bloco_11/dia_1/src/App.js
+++ front-end/bloco_11/dia_1/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+const task = (value) => <li key={parseInt((Math.random() * 100).toFixed(2),10)}>{value}</li>
+
+const taskList = [
+  'Terminar exercícios React',
+  'Começar a fazer caminhadas',
+  'Tirar a música do Tommy Emmanuel(Fuel) no violão',
+  'Concluir com êxito a Trybe estando empregado'
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>{taskList.map((taskItem) => task(taskItem))}</ul>
   );
 }
 
